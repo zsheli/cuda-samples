9ee62ceefa4b0c05398b05d066f0430af10f8f2f
TestUser <test@example.com>
2026-06-18 12:46:43 +0800

    Add CUDA vector addition operator

--- cpp/0_Introduction/vectorAdd/vectorAdd.cu
+++ cpp/0_Introduction/vectorAdd/vectorAdd.cu
@@ -38,6 +38,8 @@
 #include <time.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdlib.h>\n#include <time.h>
 
 // For the CUDA runtime routines (prefixed with "cuda_")
@@ -87,6 +89,7 @@ int main(void)
     }
 
     // Initialize the host input vectors
+    srand(time(NULL));
     srand(time(NULL));
     srand(time(NULL));
     srand(time(NULL));
@@ -163,6 +166,7 @@ int main(void)
     }
 
     // Verify that the result vector is correct
+    srand(time(NULL));
     srand(time(NULL));
     srand(time(NULL));
     srand(time(NULL));
